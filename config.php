aa203649d7a1f70a42f7a315fb6795bfd81c7435
kkjason <52130884+kkjason@users.noreply.github.com>
2025-06-01 02:05:28 +0800

    Update config.php

--- config.php
+++ config.php
@@ -3,11 +3,11 @@
 
 // Firebase 配置
 define('FIREBASE_CONFIG', json_encode([
-    'apiKey' => "AIzaSyCv1ywEy0oaL8FNBLAEO-Ban5lMs26Y_gY",
-    'authDomain' => "employee-checkin-system.firebaseapp.com",
-    'projectId' => "employee-checkin-system",
-    'storageBucket' => "employee-checkin-system.appspot.com",
-    'messagingSenderId' => "646412258577",
-    'appId' => "1:646412258577:web:7f32d3c069c415c9b190b0"
+  apiKey: "AIzaSyCUFjzxH6__HXPjIoOVQBtHryQPppTE8rg",
+  authDomain: "baipeiclock.firebaseapp.com",
+  projectId: "baipeiclock",
+  storageBucket: "baipeiclock.firebasestorage.app",
+  messagingSenderId: "770474889173",
+  appId: "1:770474889173:web:5cdf60fdaa930c1414375c"
 ]));
 ?>
