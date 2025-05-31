3cd2d528f4f42b50ccc9b68ea34a84950717bfa7
kkjason <52130884+kkjason@users.noreply.github.com>
2025-06-01 02:00:57 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+// config.php
+
+// Firebase 配置
+define('FIREBASE_CONFIG', json_encode([
+    'apiKey' => "AIzaSyCv1ywEy0oaL8FNBLAEO-Ban5lMs26Y_gY",
+    'authDomain' => "employee-checkin-system.firebaseapp.com",
+    'projectId' => "employee-checkin-system",
+    'storageBucket' => "employee-checkin-system.appspot.com",
+    'messagingSenderId' => "646412258577",
+    'appId' => "1:646412258577:web:7f32d3c069c415c9b190b0"
+]));
+?>
